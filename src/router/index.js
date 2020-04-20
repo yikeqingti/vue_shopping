95597e292efe0043405e39ab7052a4c911e80cea
yikeqingti <1939036978@qq.com>
2020-04-20 21:13:10 +0800

    登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/login.vue'
+import Home from '../components/home.vue'
+import '../assets/css/global.css'
+import axios from 'axios'
 
+Vue.prototype.$http = axios
+    //配置请求根路径
+axios.defaults.baseURL = 'ttp://127.0.0.1:8888/api/private/v1/'
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    { path: '/home', component: Home }
 ]
 
 const router = new VueRouter({
-  routes
+        routes
+    })
+    //挂载路由守卫
+router.beforeEach((to, from, next) => {
+    //to要访问的路径
+    //from 从哪个路径跳转而来
+    //next 放行
+    //next()放行  next('/login')强制跳转
+
+    if (to.path === '/login') return next();
+    const tokenStr = window.sessionStorage.getItem('token')
+    if (!tokenStr) return next('/login')
+    next()
 })
 
 export default router
